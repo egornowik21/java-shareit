fca9cebc1462dcb2729512a6acdd06b247cc2b73
egornowik <egornowik1@yandex.ru>
2023-06-06 00:00:53 +0300

    добавлены тесты для запросов и пользователей

--- src/test/java/ru/practicum/shareit/user/UserServiceTest.java
+++ src/test/java/ru/practicum/shareit/user/UserServiceTest.java
@@ -5,12 +5,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Transactional
 @SpringBootTest
@@ -67,7 +72,30 @@ public class UserServiceTest {
 
     @Test
     void deleteUser() {
+        UserDto userDto = userService.create(UserMapper.toUserDto(user));
+        user.setId(userDto.getId());
+        List<UserDto> usersList = userService.findAll();
+        userService.deleteUserById(userDto.getId());
+        List<UserDto> usersListAfterDelete = userService.findAll();
+        assertEquals(usersList.size(),usersListAfterDelete.size()+1);
+    }
 
+    @Test
+    void getUserById() {
+        UserDto userDto = userService.create(UserMapper.toUserDto(user));
+        user.setId(userDto.getId());
+        UserDto userDtoById = userService.getUserById(userDto.getId());
+        assertEquals(user.getId(),userDtoById.getId());
+    }
+
+    @Test
+    void deleteWrongUserTest() {
+        assertThrows(NotFoundException.class, () -> userService.deleteUserById(1L));
+    }
+
+    @Test
+    void getWrongUserTest() {
+        assertThrows(NotFoundException.class, () -> userService.getUserById(1L));
     }
 
 
